8ec4e0e8c3e043e23c1fa139d3f9ab0afc500871
Vishrut Reddi <vishrutreddi@gmail.com>
2019-04-06 19:17:47 -0700

    Setup project dependencies and structure (#3)
    Resolves #2

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => (
+  <div className="header">
+    <div className="header-user-circle" />
+  </div>
+);
+
+export default Header;
---
+++ webpack.config.common.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: [
+      'react-hot-loader/patch',
+      './src/index.jsx',
+    ],
+  },
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        })),
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8000,
+              name: 'images/[hash]-[name].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              webp: {
+                quality: 60,
+                lossless: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      state: path.resolve(__dirname, 'src/state'),
+      mutations: path.resolve(__dirname, 'src/mutations'),
+      assets: path.resolve(__dirname, 'src/assets'),
+      images: path.resolve(__dirname, 'src/assets/images'),
+      utils: path.resolve(__dirname, 'src/utils'),
+      queries: path.resolve(__dirname, 'src/queries'),
+    },
+    extensions: ['.js', '.jsx'],
+  },
+};
