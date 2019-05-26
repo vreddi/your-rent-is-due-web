70bd3a5487a359979451b2d985fec11f66cc0e3c
Vishrut Reddi <vishrutreddi@gmail.com>
2019-05-26 01:28:29 -0700

    Add first iteration of a tracking item card

---
+++ src/components/TrackingItem/TrackingItem.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import { createGenerateClassName } from "@material-ui/core/styles";
+// import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+// import Component from "./Component";
+
+// const TrackingItem = () => (
+//   <MuiThemeProvider
+//     theme={createMuiTheme({
+//       typography: {
+//         useNextVariants: true
+//       },
+//       overrides: Component.getTheme(muiBaseTheme)
+//     })}
+//   >
+//     <Component />
+//   </MuiThemeProvider>
+// );
